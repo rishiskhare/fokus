2dcb13a9141ef35fd409707dfa4b183e5cc7f863
Rishi Khare <rishiskhare@gmail.com>
2022-10-15 22:55:37 -0700

    Added daily quote from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Timer from "./components/Timer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
       <main>
+        <Header />
         <Timer />
       </main>
     </div>
---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+const Header = () => {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  useEffect(() => {
+    const url = "https://motivational-quote-api.herokuapp.com/quotes/random";
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setQuote(json.quote);
+        setAuthor(json.person);
+      } catch (error) {}
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Fokus</h1>
+      <h2>{quote}</h2>
+      <h2>{author}</h2>
+    </div>
+  );
+};
+
+export default Header;
